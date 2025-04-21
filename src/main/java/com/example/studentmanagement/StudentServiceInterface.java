3f50363c77c118a85588623bbcea287e5a745680
SupriyaKP1 <162010373+SupriyaKP1@users.noreply.github.com>
2025-04-21 19:41:51 +0530

    Create StudentServiceInterface.java

---
+++ src/main/java/com/example/studentmanagement/StudentServiceInterface.java
@@ -0,0 +1,10 @@
+package com.studentmanagement.system.service;
+import com.studentmanagement.system.model.Student;
+import java.util.List;
+import java.util.Optional;
+public interface StudentServiceInterface {
+    List<Student> getAllStudents();
+    Optional<Student> getStudentById(Long id);
+    Student saveStudent(Student student);
+    void deleteStudent(Long id);
+}
