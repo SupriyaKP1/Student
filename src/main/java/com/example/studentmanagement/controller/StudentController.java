f65ac22047c57b43777c8cba347e1936a6f84c3d
SupriyaKP1 <162010373+SupriyaKP1@users.noreply.github.com>
2025-04-21 19:37:44 +0530

    Update StudentController.java

--- src/main/java/com/example/studentmanagement/controller/StudentController.java
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -1,40 +1,40 @@
-package com.example.studentmanagement.controller;
-import com.example.studentmanagement.model.Student;
-import com.example.studentmanagement.repository.StudentRepository;
+package com.studentmanagement.system.controller;
+import com.studentmanagement.system.model.Student;
+import com.studentmanagement.system.service.StudentServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 @RestController
-@RequestMapping("/api/students")
-
+@RequestMapping("/api/student-management/students")
 public class StudentController {
-
     @Autowired
-    private StudentRepository repo;
-
+    private StudentServiceInterface studentService;
+    @GetMapping("/index")
+    public String getIndexPage() {
+        return "index.html";
+    }
     @GetMapping
-    public List<Student> getAll() {
-        return repo.findAll();
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
     }
-
-    @PostMapping
-    public Student create(@RequestBody Student student) {
-        return repo.save(student);
-    }
-
     @GetMapping("/{id}")
-    public Student getById(@PathVariable Long id) {
-        return repo.findById(id).orElse(null);
+    public Optional<Student> getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
     }
-
+    @PostMapping
+    public String createStudent(@RequestBody Student student) {
+        studentService.saveStudent(student);
+        return "Student inserted successfully";
+    }
     @PutMapping("/{id}")
-    public Student update(@PathVariable Long id, @RequestBody Student student) {
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
         student.setId(id);
-        return repo.save(student);
+        return studentService.saveStudent(student);
     }
-
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        repo.deleteById(id);
+    public String deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return "Student deleted successfully";
     }
 }
