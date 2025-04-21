d529c1edbf078c6e85eb204fbc78b0b0e7fb9485
SupriyaKP1 <162010373+SupriyaKP1@users.noreply.github.com>
2025-04-21 19:39:00 +0530

    Update StudentRepository.java

--- src/main/java/com/example/studentmanagement/repository/StudentRepository.java
+++ src/main/java/com/example/studentmanagement/repository/StudentRepository.java
@@ -1,6 +1,7 @@
-package com.example.studentmanagement.repository;
-import com.example.studentmanagement.model.Student;
+package com.studentmanagement.system.repository;
+import com.studentmanagement.system.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
-
